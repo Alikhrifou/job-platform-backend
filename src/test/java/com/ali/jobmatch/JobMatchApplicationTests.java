1a416930a86657a1a4796efb788c1a0733d97dce
ali-khrifou <138569202+ali-khrifou@users.noreply.github.com>
2026-02-25 14:38:28 +0000

    Initialize job platform backend with Maven setup and basic application structure

---
+++ src/test/java/com/ali/jobmatch/JobMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.jobmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
